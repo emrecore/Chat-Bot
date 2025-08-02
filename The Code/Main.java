443a41832a8944bc052c990d16850f947653603f
Emre <emre@blgn.de>
2025-08-03 01:41:29 +0200

    Added ability: Counting

--- The Code/Main.java
+++ The Code/Main.java
@@ -5,12 +5,13 @@ public class Main {
 
     static String botName;
 
+
     public static void name() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi, I am your personal Chat Bot!");
         System.out.println("Please give me a name :)");
         botName = scanner.nextLine();
-        System.out.println(botName + ":" + " " + "I like this name, Thank You! Feel free to use all of my current abilities to your liking :)");
+        System.out.println(botName + ": " + "I like this name, Thank You! Feel free to use all of my current abilities to your liking :)");
     }
 
     public static void main(String[] args) {
@@ -20,7 +21,7 @@ public class Main {
         String ability = scanner.nextLine();
 
         if (ability.isEmpty()) {
-            System.out.println("Well then, I hope I will be able to assist you another time :)");
+            System.out.println(botName + ": " + "Well then, I hope I will be able to assist you another time :)");
         }
 
         if (ability.equals("Guessing")) {
@@ -35,7 +36,7 @@ public class Main {
             quizzing();
         }
 
-        if (ability.equals("Adding0")) {
+        if (ability.equals("Adding")) {
             adding();
         }
 
@@ -54,21 +55,37 @@ public class Main {
 
     public static void guessing() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(botName + ":" + " " + "I can guess anybody's age. Please provide me with the remainders of your age after dividing by 3, 5, and 7 :)");
+        System.out.println(botName + ": " + "I can guess anybody's age. Please provide me with the remainders of your age after dividing by 3, 5, and 7 :)");
         System.out.println("Separate your numbers with whitespace");
         int remainder1 = scanner.nextInt();
         int remainder2 = scanner.nextInt();
         int remainder3 = scanner.nextInt();
         int age = (remainder1 * 70 + remainder2 * 21 + remainder3 * 15) % 105;
 
-        System.out.println(botName + ":" + " " + "I feel like your age might be " + age + " :)");
+        System.out.println(botName + ": " + "I feel like your age might be " + age + " :)");
     }
 
 
     public static void counting() {
-        System.out.println("Soon...");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(botName + ": " + "Please enter a Number :)");
+        int finalNumber = scanner.nextInt();
+
+        if (finalNumber == 0) {
+            System.out.println(botName + ": " + "There is nothing to count :(");
+            System.exit(0);
+        }
+
+        for (int startNumber = 0; finalNumber >= startNumber; startNumber++) {
+            System.out.println(startNumber);
+        }
+
+        for (int startNumber = 0; finalNumber <= startNumber; startNumber--) {
+            System.out.println(startNumber);
+        }
 
     }
+
 
     public static void quizzing() {
         System.out.println("Soon...");
@@ -94,7 +111,6 @@ public class Main {
         System.out.println("Soon...");
 
     }
-
 
 
 }
